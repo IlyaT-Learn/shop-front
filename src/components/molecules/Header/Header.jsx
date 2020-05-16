8746a28cd4b16283848c01b74825b0eb7e3b4646
Danila656 <zankevichdanya535@gmail.com>
2020-05-16 23:12:50 +0300

    markup

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import styles from './Header.module.scss'
+import {unstable_renderSubtreeIntoContainer} from "react-dom";
 
 const Header = () => {
-
-  return (
-    <div className={styles.headerContainer}>Header</div>
-  );
+    return (
+        <div className={styles.headerContainer}>
+            <div className={styles.unitWithMenu}>
+                <div className={styles.invisibleUnit}/>
+                <div className={styles.menu}>
+                    <a>Contact</a>
+                    <a>Affiliate </a>
+                    <a>Blogs</a>
+                </div>
+            </div>
+            <div className={styles.searchUnit}>
+                <div className={styles.logo}>
+                    <div className={styles.circle}/>
+                    <p className={styles.nameOfTheMarket}><strong>m</strong>artech</p>
+                </div>
+                <div className={styles.unitWithSearchField}>
+                    <input placeholder="Search Products Here" className={styles.searchField}/>
+                    <a className={styles.searchButton}/>
+                </div>
+                <a className={styles.registerButton}>Register && sign in</a>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
--- src/components/molecules/Menu/Menu.jsx
+++ src/components/molecules/Menu/Menu.jsx
@@ -3,9 +3,56 @@ import styles from './Menu.module.scss'
 
 const Menu = () => {
 
-  return (
-    <div className={styles.menuContainer}>Menu</div>
-  );
+    return (
+        <div className={styles.menuContainer}>
+            <div className={styles.mainUnit}>
+                <p>All Categories</p>
+                <p className={styles.line}/>
+            </div>
+            <div className={styles.unitWithCategories}>
+                <div>
+                    <img src="#"/>
+                    <a>Clothes & Footwear</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Kitchen Appiance</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>New Arrivals</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Laptop & Computer</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Mobile & Cover</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Accessories</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Interior Design</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Game & Sports</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>Furniture & Lighting</a>
+                </div>
+                <div>
+                    <img src="#"/>
+                    <a>More Categories</a>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Menu;
