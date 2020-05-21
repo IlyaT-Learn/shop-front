a308fb940150be4fc5dfbb962af285bd8d57534c
Danila656 <zankevichdanya535@gmail.com>
2020-05-21 15:09:48 +0300

    markup

--- src/components/atoms/FNavLink/FNavLink.jsx
+++ src/components/atoms/FNavLink/FNavLink.jsx
@@ -2,26 +2,11 @@ import React from 'react';
 import styles from './FNavLink.module.scss';
 import cn from 'classnames';
 
-const FNavLink = ({children,className}) =>{
-    const highLightNavButton = (event) => {
-        event.target.parentNode.firstChild.className = `${styles.lightArrow}`;
-        event.target.parentNode.lastChild.className =`${styles.lightNameOfNavButton}`;
-    }
-
-    const lowLightNavButton = (event) => {
-        event.target.parentNode.lastChild.className =`${styles.nameOfNavButton}`;
-        event.target.parentNode.firstChild.className = `${styles.arrow}`
-    }
-
-    return (
-        <div className={cn(styles.navButton , className)}>
-            <i onMouseOut={(event) => lowLightNavButton(event)}
-               onMouseOver={(event) => highLightNavButton(event)}
-               className={styles.arrow}/>
-            <p className={styles.nameOfNavButton} onMouseOut={(event) => lowLightNavButton(event)}
-               onMouseOver={(event) => highLightNavButton(event)}>{children}</p>
-        </div>
-    )
-}
+const FNavLink = ({children, className}) => (
+    <div className={cn(styles.navLink, className)}>
+        <i className={styles.arrow}/>
+        <p className={styles.nameOfNavLink}>{children}</p>
+    </div>
+);
 
 export default FNavLink;
