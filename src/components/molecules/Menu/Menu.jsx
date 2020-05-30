86701c684873d06ff44ce54bda629501b45f9bb9
Ilya <pochta_tii@mail.ru>
2020-05-30 03:48:15 +0300

    Changes to the menu component

--- src/components/molecules/Menu/Menu.jsx
+++ src/components/molecules/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import styles from './Menu.module.scss'
-import {SUCCESS} from "../../../constants/request.constants";
+import {SUCCESS} from '../../../constants/request.constants';
 import Category from '../../atoms/MCategory/Category';
 
 const Menu = ({getListOfCategories, menuSubmitRequest}) => {
@@ -12,13 +12,13 @@ const Menu = ({getListOfCategories, menuSubmitRequest}) => {
     return (
         <div className={styles.menuContainer}>
             <div className={styles.mainUnit}>
-                <p>All Categories</p>
+                <p>Все категории</p>
                 <p className={styles.line}/>
             </div>
             <div className={styles.unitWithCategories}>
-               {menuSubmitRequest.status === SUCCESS ? menuSubmitRequest.result.payload.allCategories .map(item => {
-                        return (<Category>{item.name}</Category>)
-                    })  : null
+                {menuSubmitRequest.status === SUCCESS ? menuSubmitRequest.result.payload.allCategories.map(item => {
+                    return (<Category>{item.name}</Category>)
+                }) : null
                 }
             </div>
         </div>
