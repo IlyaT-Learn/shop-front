a85d7a5bd4a0981f9162431426eae8411b344b10
Ilya <pochta_tii@mail.ru>
2020-05-30 04:57:26 +0300

    Making page changes

--- src/components/atoms/HRegisterOrSignInButton/HRegisterOrSignInButton.jsx
+++ src/components/atoms/HRegisterOrSignInButton/HRegisterOrSignInButton.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './HRegisterOrSignInButton.module.scss';
 
 const RegisterOrSignInButton = () => (
-        <a className={styles.registerButton}>Register && sign in</a>
-    );
+    <div className={styles.regAndLoginContainer}>
+        <a className={styles.HButton}><strong>Регистрация</strong></a>
+        <p>&</p>
+        <a className={styles.HButton}><strong>Вход</strong></a>
+    </div>
+);
 
 export default RegisterOrSignInButton;
--- src/components/atoms/HUnitWithSearchField/UnitWithSearchField.jsx
+++ src/components/atoms/HUnitWithSearchField/UnitWithSearchField.jsx
@@ -4,7 +4,7 @@ import SendButton from '../../atoms/SendButton/SendButton';
 
 const UnitWithSearchField = () => (
     <div className={styles.unitWithSearchField}>
-        <input placeholder="Search Products Here" className={styles.searchField}/>
+        <input placeholder="Глобальный поиск" className={styles.searchField}/>
         <SendButton/>
     </div>
 );
