70ca11a612f8b3e2524fc6317b14317cb989a3d9
Danila656 <zankevichdanya535@gmail.com>
2020-06-10 01:15:19 +0300

    .

--- src/page/PageWithProduct/PageWithProduct.jsx
+++ src/page/PageWithProduct/PageWithProduct.jsx
@@ -1,20 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './PageWithProduct.module.scss';
+import cn from 'classnames';
 
 const PageWithProduct = () => {
-    const displayDescription = () => {
-        document.getElementById(`description`).style.display = 'block';
-        document.getElementById('reviewsButton').style.color = 'black'
-        document.getElementById('descriptionButton').style.color = '#db5400'
-        document.getElementById(`reviews`).style.display = 'none';
-    }
 
-    const displayReviews = () => {
-        document.getElementById(`reviews`).style.display = 'block';
-        document.getElementById('descriptionButton').style.color = 'black'
-        document.getElementById('reviewsButton').style.color = '#db5400'
-        document.getElementById(`description`).style.display = 'none';
-    }
+    const [status, changeStatus] = useState(false);
 
     return (
         <div className={styles.pageContainer}>
@@ -27,8 +17,10 @@ const PageWithProduct = () => {
                         <p className={styles.nameOfTheProduct}>Имя продукта</p>
                         <div className={styles.unitWithAssessment}>
                             <p><span className={styles.boldText}>Рейтинг: </span></p>
-                            <a href="#reviews " className={styles.reviewsAnchor}>Отзывы</a>
-                            <a href="#writeReview " className={styles.writeReviewButton}>Написать отзыв</a>
+                            <a onClick={() => changeStatus(true)} href="#reviews "
+                               className={styles.reviewsAnchor}>Отзывы</a>
+                            <a onClick={() => changeStatus(true)} href="#writeReview "
+                               className={styles.writeReviewButton}>Написать отзыв</a>
                         </div>
                         <p><span className={styles.boldText}>Марка: </span></p>
                         <p><span className={styles.boldText}>Код продукта: </span></p>
@@ -49,24 +41,40 @@ const PageWithProduct = () => {
             </div>
             <div className={styles.descriptionOfProduct}>
                 <div>
-                    <a id="descriptionButton" onClick={displayDescription} className={styles.descriptionButton}>Описание</a>
-                    <a id="reviewsButton" onClick={displayReviews} className={styles.reviewsButton}>Отзывы(1)</a>
+                    <a onClick={() => changeStatus(false)}
+                       className={cn({
+                           [styles.selectedSectionButton]: !status,
+                           [styles.sectionButton]: status
+                       })}>Описание</a>
+                    <a className={cn({
+                        [styles.selectedSectionButton]: status,
+                        [styles.sectionButton]: !status
+                    })}
+                       onClick={() => changeStatus(true)}>Отзывы(1)</a>
                 </div>
-                <div id="description" className={styles.description}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                <div className={cn({
+                    [styles.description]: !status,
+                    [styles.invisibleUnit]: status
+                })}>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                     eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                     exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
                     reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
                     occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </div>
-                <div id="reviews" className={styles.reviews}>
-                    <p id="writeReview"  className={styles.writeReviewText}>Написать отзыв</p>
+                <div id="reviews" className={cn({
+                    [styles.invisibleUnit]: !status,
+                    [styles.reviews]: status
+                })}>
+                    <p id="writeReview" className={styles.writeReviewText}>Написать отзыв</p>
                     <p className={styles.reviewUserInfo}><span className={styles.redText}>*</span>Ваше имя</p>
                     <input className={styles.writeReviewField}/>
                     <p className={styles.reviewUserInfo}><span className={styles.redText}>*</span>Ваш отзыв</p>
                     <input className={styles.writeReviewField}/>
-                    <p className={styles.reviewUserInfo}><span className={styles.redText}>*</span>Ваша оценка продукта</p>
+                    <p className={styles.reviewUserInfo}><span className={styles.redText}>*</span>Ваша оценка продукта
+                    </p>
                     <input className={styles.writeAssessmentField} placeholder="10"/>
-                    <p >Отзывы</p>
+                    <p>Отзывы</p>
                 </div>
             </div>
         </div>
